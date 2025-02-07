a7e89ace87d9aed3b2957152a1d971f15c2f3ca1
Michał Jakubowski <revanmj@gmail.com>
2025-02-07 16:26:36 +0100

    Update assistant tile's code for newer SDKs

--- app/src/main/java/pl/revanmj/qtassist/AssistantTileService.java
+++ app/src/main/java/pl/revanmj/qtassist/AssistantTileService.java
@@ -1,7 +1,7 @@
 package pl.revanmj.qtassist;
 
+import android.app.Dialog;
 import android.app.SearchManager;
-import android.content.Intent;
 import android.os.Bundle;
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
@@ -26,10 +26,16 @@ public class AssistantTileService extends TileService {
 
         SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
         try {
-            Intent dismissNotificationShade = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
-            this.getApplicationContext().sendBroadcast(dismissNotificationShade);
+            // Below part is needed so that quick tiles tray gets closed
+            // and a screenshot passed to the digital assistant contains screen underneath the tray.
+            Dialog dialog = new Dialog(this);
+            showDialog(dialog);
+            dialog.dismiss();
+            Thread.sleep(500);
+
             SearchManager.class.getMethod("launchAssist", Bundle.class).invoke(searchManager, new Bundle());
-        } catch(NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+        } catch(NoSuchMethodException | IllegalAccessException | InvocationTargetException |
+                InterruptedException e) {
             Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
         }
     }
